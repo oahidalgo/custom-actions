78e824cf008ba3b54ce16559116645c055016d48
oahidalgo <ohgovilla@gmail.com>
2024-08-27 15:17:11 -0600

    UPD - Solving sync problems

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,20 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  //1) Get some input values
+  // 1) Get some input values
   const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
-  // 2) Upload files to S3
-  core.notice('Hello from my custom JavaScript action');
-  core.notice(`Syncing ${distFolder} to s3://${bucket} in ${bucketRegion}`);
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  exec.exec(
-    `aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`
-  );
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // echo 'website-url=...' >> $GITHUB_OUTPUT
 }
 
 run();
