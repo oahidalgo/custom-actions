3ab2f4e4e57c0afaef271fbcfacf21503e534771
oahidalgo <ohgovilla@gmail.com>
2024-08-27 07:57:32 -0600

    UPD - Log the variables

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,12 +9,12 @@ function run() {
   const distFolder = core.getInput('dist-folder', { required: true });
 
   // 2) Upload files to S3
+  core.notice('Hello from my custom JavaScript action');
+  core.notice(`Syncing ${distFolder} to s3://${bucket} in ${bucketRegion}`);
 
   exec.exec(
     `aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`
   );
-
-  core.notice('Hello from my custom JavaScript action');
 }
 
 run();
