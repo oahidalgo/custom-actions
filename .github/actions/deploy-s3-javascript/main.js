b88572a53e995c5de59e67bed120f91a99cebfbf
oahidalgo <ohgovilla@gmail.com>
2024-08-27 07:40:55 -0600

    ADD - Added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  //1) Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files to S3
+
+  exec.exec(
+    `aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`
+  );
+
   core.notice('Hello from my custom JavaScript action');
 }
 
